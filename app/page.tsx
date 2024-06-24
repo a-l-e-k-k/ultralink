f9f1f75c7bb392f8548bce4a42fb3b012e355daa
a-l-e-k-k <aleksander.klimczyk05@gmail.com>
2024-06-23 23:36:57 -0500

    basic template

--- app/page.tsx
+++ app/page.tsx
@@ -261,8 +261,8 @@ const Contact = () => (
 );
 
 const Footer = () => (
-  <footer className="bg-black border-t border-gray-800">
-    <div className="max-w-7xl mx-auto py-8 px-4 sm:px-6 lg:px-8 text-center text-gray-400">
+  <footer className="bg-black border-t pt-9 border-gray-800">
+    <div className="max-w-7xl mx-auto py-8 px-4  sm:px-6 lg:px-8 text-center text-gray-400">
       <p>&copy; 2024 ULTRALINK. Forging the synapses of tomorrow&apos;s digital ecosystem.</p>
     </div>
   </footer>
